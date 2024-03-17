936053cf49e5f32ec45cf8a518dab2fad52b8271
Bohdan99I <bohdan.voiyov@gmail.com>
2024-03-17 11:37:16 +0200

    add_ofline+Node

--- task#1.js
+++ task#1.js
@@ -1,12 +1,29 @@
 const base64 = require("base-64");
 
-// Масив з назвами етапів життєвого циклу розробки програмного забезпечення
 const steps = ["plan", "code", "test", "delivery", "deploy", "monitor"];
 
-// Цикл для обробки кожного елемента масиву
-steps.forEach((step) => {
-  // Кодуємо поточний етап у форматі base64
-  const encodedStep = base64.encode(step);
-  // Виводимо закодований етап на консоль
+steps.forEach((step) => {  
+  const encodedStep = base64.encode(step); 
   console.log(encodedStep);
 });
+
+// Без зовнішнього підключення
+//const steps = ["plan", "code", "test", "delivery", "deploy", "monitor"];
+// Цикл для обробки кожного елемента масиву
+//steps.forEach(step => {
+    // Кодуємо поточний етап у форматі base64
+    //const encodedStep = btoa(step);
+    // Виводимо закодований етап на консоль
+    //console.log(encodedStep);
+//});
+
+
+// Код поза середовищем браузера, наприклад, у середовищі Node.js
+//const steps = ["plan", "code", "test", "delivery", "deploy", "monitor"];
+// Цикл для обробки кожного елемента масиву
+//steps.forEach(step => {
+    // Кодуємо поточний етап у форматі base64
+    //const encodedStep = Buffer.from(step).toString('base64');
+    // Виводимо закодований етап на консоль
+    //console.log(encodedStep);
+//});
